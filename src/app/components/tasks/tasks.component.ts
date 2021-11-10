16f95571e8bd75a4ee8364480819473d4b194191
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2021-11-10 11:17:08 +0530

    Implemented Delete Task

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -18,4 +18,12 @@ export class TasksComponent implements OnInit {
     );  
   }
 
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(
+      () => (
+        this.tasks = this.tasks.filter(t => t.id !== task.id)
+      )
+    )
+  }
+
 }
