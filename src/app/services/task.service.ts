33ca6bf5b1c041a25a5eb00260c029a8a2a8a4d0
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2021-11-10 10:52:37 +0530

    Angular HTTP Client setup

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { TASKS } from '../mock-tasks';
 import { Task } from '../Task';
@@ -7,6 +8,7 @@ import { Task } from '../Task';
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = "http://localhost:5000/tasks";
 
   constructor() { }
 
