1116a0e65a64f2429f0669413b7179d9cc055791
Sarthak Ladhwe <sarthak.ladhwe156@gmail.com>
2021-11-16 11:04:46 +0530

    Toggle Button & UI Service

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { Task } from '../../Task'
+import { Subscription } from 'rxjs';
+import { Task } from '../../Task';
+import { UiService } from '../../services/ui.service';
 
 @Component({
   selector: 'app-add-task',
@@ -11,9 +13,14 @@ export class AddTaskComponent implements OnInit {
   text: string = "";
   day: string = "";
   reminder: boolean = false;
-  
+  showAddTask!: boolean;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(value =>
+      this.showAddTask = value
+    );
+  }
 
   ngOnInit(): void {
   }
